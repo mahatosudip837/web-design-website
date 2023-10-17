9d1153df0e5b8a49069124480321a029c8e237a2
SUDIP MAHATO <mahatosudip837@gmail.com>
2023-10-17 20:12:21 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){
+    // hero slider 
+    $('#hero-slider').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        items:1,
+        dots:false,
+        smartSpeed:1000,
+        autoplay:true,
+        autoplayTimeout:5000
+    });
+    $('.counter').counterUp({
+        delay: 10,
+        time: 3000
+        });
+    // portfolio slider 
+    $('#project-slider').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:false,
+        dots:true,
+        smartSpeed:1000,
+        margin:24,
+        autoplay:true,
+        autoplayTimeout:3000,
+        responsive:{
+            0:{
+                items:1,
+                margin:0,
+            },
+            768:{
+                items:2,
+            },
+            1140:{
+                items:2,
+                center:true,
+            }
+        }
+    });
+
+    // reviews slider 
+    $('#reviews-slider').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        dots:true,
+        items:1,
+        autoplay:true,
+        autoplayTimeout:2000,
+        smartSpeed:1000
+    })
+});
